bc025a5ba7caed0757ff7368ca0ec3111e4e5f97
eugene-bakulin <bakulin.eugene@gmail.com>
2022-05-18 22:01:34 +0400

    feat: implement task 5

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -2,23 +2,28 @@ const fs = require('fs');
 const path = require('path');
 const { stdout } = process;
 
-fs.readdir(path.join(__dirname, 'styles'), function (err, items) {
+fs.readdir(path.join(__dirname, 'styles'), {withFileTypes: true}, function (err, items) {
     if (err) {
         throw err;
     } else {
-        const onlyCSS = items.filter(item => item.isFile() && path.extname(item) === '.css');
+        fs.writeFile(path.join(__dirname, 'project-dist', 'bundle.css'),'', err => {if (err) throw err});
+        const onlyCSS = items.filter(item => {
+            if (item.isFile() && (path.extname(item.name) === '.css')) {
+                return true;
+            }
+        });
         onlyCSS.forEach(file => {
-            const readStream = fs.createReadStream(path.join(__dirname, 'styles', file));
+            const readStream = fs.createReadStream(path.join(__dirname, 'styles', file.name));
             let data = '';
             readStream.on('data', chunk => {
                 data += chunk;
             });
             readStream.on('end', () => fs.appendFile(path.join(__dirname, 'project-dist', 'bundle.css'),
-            data,
+            data + '\n',
             (err) => {
                 if (err) throw err;
-                stdout.write('bundle.css created!');
             }))
-        });     
+        });
     }
+    stdout.write('bundle.css with merged styles created!\n');
 });
